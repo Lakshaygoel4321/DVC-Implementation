94fd7f8d35a3f6e03d07259960add185204f3aad
Lakshaygoel4321 <iamlakshaygoel5990@gmail.com>
2025-02-12 17:16:37 +0530

    before commit the dvc

---
+++ mycode.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+df = pd.DataFrame({
+    "Name":["Ayuhs","Vasu","Lakshay"],
+    "city":["UP","UK","Sonipat"],
+    "age":[23,22,22]
+})
+
+dir_data = "data"
+
+os.makedirs(dir_data,exist_ok=True)
+
+file_path = os.path.join(dir_data,"sample_data.csv")
+
+df.to_csv(file_path,index=False)
+
+print(f"CSV file is saved: {file_path}")
